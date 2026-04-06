9645ae56bdc3d276bb16a04d659070e4563a2ed9
NolanFrater <nolanfrater6@gmail.com>
2026-04-06 15:12:50 -0500

    Add files via upload

---
+++ simplemadlib.py
@@ -0,0 +1,17 @@
+#(Animal) is the most (adjective) thing in the (large place).
+# It is known for (action verb ending in "ing") around (other animal (plural)).
+# It can often be seen near (adjective) (noun (plural)).
+# This animal is often considered (adjective) and (adjective) by many people, but it is actually extremely (adjective).
+
+Animal_1 = input("Animal: \n")
+Animal_2 = input("Another animal (plural): \n")
+Adjective_1 = input("Adjective: \n")
+Adjective_2 = input("Adjective: \n")
+Adjective_3 = input("Adjective: \n")
+Adjective_4 = input("Adjective: \n")
+Adjective_5 = input("Adjective: \n")
+Location = input("Location: \n")
+Verb = input("Action verb ending in 'ing': \n")
+Noun = input("Noun (plural): \n")
+
+print(Animal_1 + " is the most " + Adjective_4 + " thing in the " + Location + ".\n" + "It is known for " + Verb + " around " + Animal_2 + ".\n" + "It can often be seen near " + Adjective_3 + " " + Noun + ".\n" + "This animal is often considered " + Adjective_2 + " and " + Adjective_5 + " by many people, but it is actually extremely " + Adjective_1 + ".")
